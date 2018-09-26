8868082d7706b37796da10957b4b9023946de14d
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-26 21:58:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,16 +4,16 @@ def third_challenge
       :patriarch => {:name => "Lord Montague", :age => "53"
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {:name => "Lady Montague", :age => "54"
         #your key/value pairs here
         },
-      :hero => {
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {:name =>
         #your key/value pairs here
         },
       :matriarch => {
